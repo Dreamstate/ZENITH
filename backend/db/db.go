f5170d43ff31e1aeb35d76596daf819b1b2f7ee5
nick <nick920330@hotmail.co.uk>
2024-12-12 02:20:21 +0200

    [BE]get gin and tidy vendor

--- backend/db/db.go
+++ backend/db/db.go
@@ -1,43 +1,35 @@
 package db
 
-import (
-	"log"
+// func stuff() {
+// 	dsn := "host=localhost user=zenith password=zenith dbname=zenith port=5432 sslmode=disable TimeZone=UTC"
+// 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		log.Fatalf("Failed to connect to database: %v", err)
+// 	}
 
-	"github.com/golang-migrate/migrate/v4"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-)
+// 	// Apply migrations
+// 	sqlDB, err := db.DB()
+// 	if err != nil {
+// 		log.Fatalf("Failed to get database connection: %v", err)
+// 	}
 
-func stuff() {
-	dsn := "host=localhost user=zenith password=zenith dbname=zenith port=5432 sslmode=disable TimeZone=UTC"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
+// 	driver, err := postgres.WithInstance(sqlDB, &postgres.Config{})
+// 	if err != nil {
+// 		log.Fatalf("Failed to create migration driver: %v", err)
+// 	}
 
-	// Apply migrations
-	sqlDB, err := db.DB()
-	if err != nil {
-		log.Fatalf("Failed to get database connection: %v", err)
-	}
+// 	m, err := migrate.NewWithDatabaseInstance(
+// 		"file://db/migrations",
+// 		"postgres", driver,
+// 	)
+// 	if err != nil {
+// 		log.Fatalf("Failed to create migrate instance: %v", err)
+// 	}
 
-	driver, err := postgres.WithInstance(sqlDB, &postgres.Config{})
-	if err != nil {
-		log.Fatalf("Failed to create migration driver: %v", err)
-	}
+// 	err = m.Up()
+// 	if err != nil && err != migrate.ErrNoChange {
+// 		log.Fatalf("Failed to apply migrations: %v", err)
+// 	}
 
-	m, err := migrate.NewWithDatabaseInstance(
-		"file://db/migrations",
-		"postgres", driver,
-	)
-	if err != nil {
-		log.Fatalf("Failed to create migrate instance: %v", err)
-	}
-
-	err = m.Up()
-	if err != nil && err != migrate.ErrNoChange {
-		log.Fatalf("Failed to apply migrations: %v", err)
-	}
-
-	log.Println("Migrations applied successfully!")
-}
+// 	log.Println("Migrations applied successfully!")
+// }
